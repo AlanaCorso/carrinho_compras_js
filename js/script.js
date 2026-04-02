f74a452260835eb2741b0002956f4587de80d65c
AlanaCorso <alana.corso@aluno.senai.br>
2026-04-02 12:17:23 -0300

    README finalizado
    (menos imagem final do projeto)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-// Digitar aqui o código do script.js
+// Inicialização de variáveis
+let cart = [];
+let modalQt = 0;
+let key = 0;
+
+// Função auxiliar para selecionar um único elemento do DOM
+const c = (el) => document.querySelector(el);
+
+// Função auxiliar para selecionar vários elementos do DOM
+const cs = (el) => document.querySelectorAll(el);
+
+// Manipulação dos dados do modelo a partir do JSON
+modelsJson.map((item, index) => {
+    // Clonar o item do modelo
+    let modelsItem = c('.models .models-item').cloneNode(true);
+
+    // Atualizar os dados do item do modelo clonado 
+    modelsItem.setAttribute('data-key', index);
+    modelsItem.querySelector
+})
